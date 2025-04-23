135f95527292822206336953a1260dca40852405
Aenay <naywintun415@gmail.com>
2025-04-23 16:42:43 +0700

    raw authentication system add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 
 Route::view('/', 'welcome');
@@ -9,5 +11,10 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 
