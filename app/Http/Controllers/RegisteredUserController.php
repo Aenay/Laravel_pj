aed69fc4459a28c64586b03d873cc3f9b13ebba1
Aenay <naywintun415@gmail.com>
2025-04-26 23:20:07 +0700

    authentication  system completed

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
     public function create()
     {
-       return view('auth.register');
+        return view('auth.register');
     }
 
+    public function store()
+    {
+        $attribute = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6)->letters()->numbers()->symbols(), 'confirmed'],
+
+
+        ]);
+
+        $user = User::create($attribute);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
+
+    }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -12,7 +15,24 @@ class SessionController extends Controller
     }
     public function store()
     {
-        dd(request()->all());
+       $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if (! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials do not matched.'
+            ]);
+
+        }
+        request()->session()->regenerate();
+
+        return redirect('/jobs');
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 
 }
